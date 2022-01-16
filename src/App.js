1de1503961c4c2268615f25c07d28f326eac4898
Alan <alan@reontel.com>
2022-01-16 23:08:53 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import FormInput from "./FormInput.js";
 
 function App() {
   return (
     <div className="App">
       <h1>Sign Up</h1>
+      <FormInput label="First Name" />
+      <FormInput label="Last Name" />
     </div>
   );
 }
